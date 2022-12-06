ffb0ff32391d95f5a1e018f90396d0eeb8eae478
juancho <jm.ticona.p@gmail.com>
2022-12-06 11:37:08 -0500

    FIX profiles

--- src/main/java/com/nocontry/ecommerce/EcommerceApplication.java
+++ src/main/java/com/nocontry/ecommerce/EcommerceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
@@ -31,7 +32,7 @@ public class EcommerceApplication {
         SpringApplication.run(EcommerceApplication.class, args);
     }
 
-    //@Bean
+    @Bean @Profile("dev")
     CommandLineRunner run(
             UserService userService,
             CategoryRepository categoryRepository,
